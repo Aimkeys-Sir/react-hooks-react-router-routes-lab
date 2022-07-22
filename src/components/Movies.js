efd446b2f264988d51846e0a0b73c808c37308a6
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-07-22 11:10:38 +0300

    commit tree 445c048ec866dd4331ecf43770837e98e0fe4996

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  console.log(movies)
+  return <div>
+    <h1>Movies Page</h1>
+    {movies.map(movie=>{
+      return (
+        <div key={movie.title}>
+          <h3>{movie.title}</h3>
+          <h4>{movie.time} min</h4>
+          <ul>
+            {movie.genres.map((genre,index)=>{
+              return <li key={index}>{genre}</li>
+            })}
+          </ul>
+          </div>
+      )
+      
+    })}
+    <ul></ul>
+  </div>;
 }
 
 export default Movies;
